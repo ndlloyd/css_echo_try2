6416f6cc675ac6bc60a04c6b6d7c0f593da890e0
Nathan Lloyd <nlloyd@smumn.edu>
2013-02-03 00:47:43 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CssEchoTry2::Application.config.secret_token = 'e54c0c36cad3e6e7cad239c7741cec69d32cc445478a81281be480845913c8dde052158cf65691b853ac94866ad8351bbcd015bdabc1599c0f33624f114219bc'
