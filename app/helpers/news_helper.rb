2248f0c59458d8f1331f4ef70c02fdcceffebc86
Nathan Lloyd <nlloyd@smumn.edu>
2013-02-12 23:11:07 -0600

    split up file and and added db

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -1,2 +1,11 @@
 module NewsHelper
+  def news_table
+    table_data = '<table class="table table-striped"  border="1" cellspacing="1" cellpadding="1"><th><span class="label label-important">Requirements</span> </th>'
+	table_data += "<tr> <td>Description</td> <td>Points</td> </tr>"
+	Requirement.find_each do |u|
+	  table_data += "<tr> <td> #{u.id} #{u.description} </td> <td> #{u.points} </td> </tr> "
+	end
+	table_data += "</table>"
+	table_data.html_safe
+  end
 end
