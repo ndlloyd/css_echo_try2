57ec4f028d2b316ca1e1f016fada42ab11859e99
Nathan Lloyd <nlloyd@smumn.edu>
2013-02-12 23:17:53 -0600

    seed string too long

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 Requirement.create!(:description => 'You are going to need to generate a News controller with a single action, display.', :points => 2 )
 Requirement.create!(:description => 'Copy your public/news.html into appropriate view directory/file', :points => 2 )
 Requirement.create!(:description => 'Gut your page! Remove text associated with four news stories, sidebar and note (at bottom).', :points => 90 )
-Requirement.create!(:description => 'Replace news text with references to appropriate controller instance variables. Your controller will now provide the news when the page is loaded.  You can fake text; anything will do. For example, in my version, I simply render "Football News" under football tab.', :points => 2 )
+Requirement.create!(:description => 'Replace news text with references to appropriate controller instance variables. Your controller will now provide the news when the page is loaded.  You can fake text; anything will do.', :points => 2 )
 Requirement.create!(:description => 'You can keep the image in the html even though it is technically related to the specific story. You must use the image_tag helper to render the image. See APIDock. There are plenty of good examples.', :points => 2 )
 Requirement.create!(:description => 'Create scaffolding for a Requirement model that contains two properties, a description (string) and points (integer). ', :points => 3 )
 Requirement.create!(:description => 'Use the scaffolding generated views pages to add the requirements to database. You need not add all requirements; I will be convinced you have accomplished your task if I see three or four. ', :points => 3 )
